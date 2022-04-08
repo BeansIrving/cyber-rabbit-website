b6c7a134c74b97b90fbf81460be1e2550d696552
Vince <sikatunar@gmail.com>
2022-04-08 11:29:00 +0800

    a

--- src/Components/Roadmap/index.js
+++ src/Components/Roadmap/index.js
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Roadmap = () => {
   return (
-    <>
-    </>
+    <RoadmapContainer>
+        <RoadmapBg>
+
+        </RoadmapBg>
+
+        <RoadmapContant>
+            <RoadmapRow>
+                <RoadmapColumn>
+                    
+                </RoadmapColumn>
+            </RoadmapRow>
+        </RoadmapContant>
+    </RoadmapContainer>
   )
 }
 
