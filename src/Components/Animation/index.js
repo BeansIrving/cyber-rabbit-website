9af2c76419bd1ca199dcff02116eb9c8d4aef30a
BinZ <sikatunar@gmail.com>
2022-04-14 18:06:13 +0800

    a

---
+++ src/Components/Animation/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { AnimationContainer, AnimationBg, AnimationContent,
+        AnimationRow, AnimationColumn, } from "./AnimationElements"
+
+
+const Animation = () => {
+  return (
+    <AnimationContainer>
+        <AnimationBg>
+
+        </AnimationBg>
+
+        <AnimationContent>
+            <AnimationRow>
+                <AnimationColumn>
+
+                </AnimationColumn>
+            </AnimationRow>
+        </AnimationContent>
+    </AnimationContainer>
+  )
+}
+
+export default Animation
