934e70fa3e1ad405db00449fc225ab95efe10aea
Vince <sikatunar@gmail.com>
2022-04-15 23:26:57 +0800

    a

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+        <FooterBg>
+
+        </FooterBg>
+
+        <FooterContent>
+            <FooterRow>
+                <FooterColumn>
+                    
+                </FooterColumn>
+            </FooterRow>
+        </FooterContent>
+    </FooterContainer>
+  )
+}
+
+export default Footer
