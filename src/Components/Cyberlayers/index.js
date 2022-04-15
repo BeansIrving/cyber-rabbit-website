19cf518707fe4a7aef242d7a347b847b62b77968
BinZ <sikatunar@gmail.com>
2022-04-15 18:52:32 +0800

    a

---
+++ src/Components/Cyberlayers/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { CyberContainer, CyberBg, CyberContent, CyberRow, CyberColumn } from "./CyberLayersElements"
+const CyberLayers = () => {
+  return (
+    <CyberContainer>
+        <CyberBg>
+
+        </CyberBg>
+
+        <CyberContent>
+            <CyberRow>
+                <CyberColumn>
+
+                </CyberColumn>
+            </CyberRow>
+        </CyberContent>
+    </CyberContainer>
+  )
+}
+
+export default CyberLayers
