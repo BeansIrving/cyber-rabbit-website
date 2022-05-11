65fc8b614fb4e44deac81002e7d78fa9701b01d8
BinZ <sikatunar@gmail.com>
2022-05-12 06:31:19 +0800

    a

---
+++ src/Components/Minting/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Minting = () => {
+  return (
+    <MintingContainer>
+        <MintingBg>
+
+        </MintingBg>
+
+        <MintingContent>
+            <MintingRow>
+                <MintingColumn>
+                    
+                </MintingColumn>
+            </MintingRow>
+        </MintingContent>
+    </MintingContainer>
+  )
+}
+
+export default Minting
--- src/Pages/mintingpage.js
+++ src/Pages/mintingpage.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../Components/Navbar'
 import Sidebar from '../Components/Sidebar'
 import Minting from '../Components/Minting'
+import Footer from '../Components/Footer'
 const mintingpage = () => {
   return (
     <>
         <Navbar />
-        <Sidebar />
+      
         <Minting />
+   
     </>
   )
 }
