188f6b764ff97cc9d8023c5b3cbf1aa7413c846c
BinZ <sikatunar@gmail.com>
2022-05-12 06:23:07 +0800

    a

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './Pages/index'
+import Minting from './Pages/mintingpage'
 function App() {
   return (
     <Router>
     
       <Switch>
         <Route path="/" component={Home} exact />
+        <Route path="/mintingpage" component={Minting} exact />
+
       </Switch>
   
    </Router>
---
+++ src/Pages/mintingpage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Sidebar from '../Components/Sidebar'
+import Minting from '../Components/Minting'
+const mintingpage = () => {
+  return (
+    <>
+        <Navbar />
+        <Sidebar />
+        <Minting />
+    </>
+  )
+}
+
+export default mintingpage
