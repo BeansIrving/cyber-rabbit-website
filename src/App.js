b46f84a9ecd7d8a870f09e305f6f695dd824ad4f
BinZ <sikatunar@gmail.com>
2022-05-12 06:23:27 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <Router>
     
       <Switch>
-        <Route path="/" component={Home} exact />
+        {/* <Route path="/" component={Home} exact /> */}
         <Route path="/mintingpage" component={Minting} exact />
 
       </Switch>
