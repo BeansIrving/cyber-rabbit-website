4867368894e73ba7cd38103b654f6092d82df2c9
BinZ <sikatunar@gmail.com>
2022-06-27 17:25:59 +0800

    Update index.js

--- src/Components/Features/index.js
+++ src/Components/Features/index.js
@@ -39,8 +39,10 @@ const Features = () => {
     useEffect(() =>{
         Aos.init({ });
       },[])
-    const [thumbsSwiper, setThumbsSwiper] = useState(null);
-    const card = { GenesisCollection, card2, card3, card4, card5, card6, card7, card8, card9 }
+const [thumbsSwiper, setThumbsSwiper] = useState(null);
+const card = { GenesisCollection, card2, card3, card4, card5, card6, card7, card8, card9 }
+
+
 
 let [selected, setSelected] = useState(card.GenesisCollection);
 let [textGenesis, setTextGenesis] = useState("Cyber Rabbit will release a Genesis collection of 1000 3D-rendered depictions of rabbits in futuristic attires with its very own utilities");
